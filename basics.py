cfaea5d12036e1e11d3035188eb4e242a4e9f2c4
iPelino <pelmut2000@gmail.com>
2023-10-31 11:00:21 +0200

    python basics

---
+++ basics.py
@@ -0,0 +1,30 @@
+# print('let\'s begin')
+# integers
+number = 5
+# Number = complex(5, i)
+# floats
+number_two = 5.0
+# bool
+is_tested = False
+
+# string
+name = "Information technology"
+
+# print(name[0:10:5])
+# print(name[5:-1]) # normal slicing
+# print(name[-5:-1]) # reverse slicing
+# print(name[:])
+# print(name[::-1])
+# print(name[:-1])
+
+# print(name.split(" "))
+# print(name.find("y"))
+# print("x" in name)
+# print(name[50])
+
+number_one = float(input("enter a number: "))
+number_two = float(input("enter another number: "))
+print(f"{number_one} + {number_two} = {number_one + number_two}")
+# country = input("enter your country: ")
+# city = input("Enter your city: ")
+# print(f"Your country is {2+2} and your city is {city}")
